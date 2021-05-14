58d1f99129fc14c9781b70fbdb6d1c025fcea804
BuildTools <unconfigured@null.spigotmc.org>
2021-05-13 20:57:39 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('topic1').addEventListener('click', function () {
+    window.location = '#topicOneText';
+})
+
+document.getElementById('topic2').addEventListener('click', function () {
+    window.location = '#topicTwoText';
+})
+
+document.getElementById('topic3').addEventListener('click', function () {
+    window.location = '#topicThreeText';
+})
+
+document.getElementById('homeicon').addEventListener('click', function() {
+    window.location = '#top'
+})
